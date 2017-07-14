ea274dfb3a59b79e02eb33c5248e4d7fc27061f1
2324451529@qq.com <AHQH1231928LZHF>
2017-07-14 11:10:05 +0800

    s1

--- app/src/main/java/com/example/testing/myapplication/MainActivity.java
+++ app/src/main/java/com/example/testing/myapplication/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.testing.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="11" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: 1");
     }
 }
