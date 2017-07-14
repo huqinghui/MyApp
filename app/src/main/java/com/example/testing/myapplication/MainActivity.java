869f49f96c582a0749518b4d7d34f6e34454e84d
2324451529@qq.com <AHQH1231928LZHF>
2017-07-14 11:15:49 +0800

    s1

--- app/src/main/java/com/example/testing/myapplication/MainActivity.java
+++ app/src/main/java/com/example/testing/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "onCreate: 1");
+        Log.i(TAG, "onCreate: version1.0");
     }
 }
